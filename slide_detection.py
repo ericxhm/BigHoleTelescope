28ad4156a2e26bbd17e6c07535dd708cc49c6670
Fluke-t <54915396+Fluke-t@users.noreply.github.com>
2020-06-29 16:51:51 -0700

    Update slide_detection.py
    changed image arrays used to calculate norm from rgb 3d arrays to grayscale array

--- slide_detection.py
+++ slide_detection.py
@@ -3,56 +3,69 @@ import matplotlib.pyplot as plt
 # %matplotlib inline
 import numpy as np
 
-### video capture to get fps, # of frames, and first frame ###
-vidcap = cv2.VideoCapture('Lec_examp.mp4')
-success = True
+def main():
+    ### video capture to get fps, # of frames, and first frame ###
+    vidcap = cv2.VideoCapture('Lec_examp.mp4')
+    success = True
 
-# interval = 5610 # just to take screenshot at 6:14 mark of video
+    # interval = 5610 # just to take screenshot at 6:14 mark of video
 
-frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))   # number of frames
-fps = vidcap.get(cv2.CAP_PROP_FPS)                   # frame rate
+    frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))   # number of frames
+    fps = vidcap.get(cv2.CAP_PROP_FPS)                   # frame rate
 
-numsec = frames//fps                                 # duration of video (sec)
-
-
-rval, frame = vidcap.read()                          # gets first frame
-first_frame = frame
-vidcap.release()
+    numsec = frames//fps  # duration of video (sec)
 
 
-manual = 12*60 + 29
-print(numsec, 'seconds, ', manual,'seconds, ',frames, 'frames, ', fps, 'fps')
+    rval, frame = vidcap.read()  # gets first frame
+    first_frame = frame
+    vidcap.release()
 
-plt.imshow(first_frame)
 
-scr_shot = np.zeros((first_frame.shape))             # creates array to save previous image in
-interval = 15                                        # take screenshot every second
+    manual = 12*60 + 29
+    print(numsec, 'seconds, ', manual,'seconds, ',frames, 'frames, ', fps, 'fps')
 
-### video capture to go through whole video
-vidcap = cv2.VideoCapture('Lec_examp.mp4')
-success = True
-start = 0                                            # variable to check if this is the first frame
-slides = []                                          # list to add time stamps and norm values to
-f = 0                                                # current frame value
-prev = 0                                             # previous frame value
+    plt.imshow(first_frame)
 
-while success:
-    success,image = vidcap.read()
-    if (f%interval == 0):
-        new = np.array(image)
-        diff = np.subtract(scr_shot,new)
-        norm = np.linalg.norm(diff)
-        if(start == 0):
-            start += 1
-        else:
-            slides.append(((prev//fps,f//fps),norm))
+    ### converting image into grayscale image (np array) ###
+    ff = np.array(first_frame)
+    d = np.array([0.2989, 0.5870, 0.1140])
+
+    gff = np.dot(ff, d) #grayscale version of first frame
+
+    ### plotting in jupyter notebook ###
+    # fig, (ax1, ax2) = plt.subplots(2, 1)
+    # ax1.imshow(ff)
+    # ax2.imshow(gff,cmap = plt.get_cmap("gray"))
+
+    scr_shot = gff
+    interval = 15                              # take screenshot every second
+
+    ### video capture to go through whole video
+    vidcap = cv2.VideoCapture('Lec_examp.mp4')
+    success = True 
+    start = 0   # variable to check if this is the first frame
+    slides = [] # list to add time stamps and norm values to
+    f = 0       # current frame value
+    prev = 0    # previous frame value
+
+    while success:
+        success, image = vidcap.read()
+        if (f%interval == 0):
+            new = np.array(image)
+            gnew = np.dot(new, d)
+            diff = np.subtract(scr_shot, gnew)
+            norm = np.linalg.norm(diff)
+            slides.append(((prev//fps, f//fps), norm))
             prev = f
+            
+        f += 1 
+        scr_shot = gnew
         
-    f +=1
-    scr_shot = image
-    
-    
-### create txt file of frames and norms ###    
-with open("norms.txt", "w") as output:
-    for listitem in slides:
-        output.write('%s\n' % str(listitem))
+        
+    ### create txt file of frames and norms ###    
+    with open("norms_1.2.txt", "w") as output:
+        for listitem in slides:
+            output.write('%s\n' % str(listitem))
+
+
+main()
