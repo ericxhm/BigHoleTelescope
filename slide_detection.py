d47bc4090443ce5afa5182323253203b5b4c0774
Fluke-t <54915396+Fluke-t@users.noreply.github.com>
2020-07-08 16:43:46 -0400

    Update slide_detection.py
    added method that checks for re occurrence of a slide every time the slides change. Prints a list of norms for every detected slide change compared to previous slides. 
    
    things to look at:
    Takes a long time (too long?) and edge case where slide is re detected after having gone backwards.

--- slide_detection.py
+++ slide_detection.py
@@ -3,9 +3,38 @@ import matplotlib.pyplot as plt
 # %matplotlib inline
 import numpy as np
 
+SLIDEENDS = []
+
+RUN = 1
+
+# function to check if new slide should be saved
+# x = new slide, y = end of prev slide
+# returns true if slide is not a repeat, false otherwise
+def new_checks(x,y):
+    if (len(SLIDEENDS) == 0):
+        SLIDEENDS.append(y)
+        return True
+    else:
+        diffs = np.subtract(SLIDEENDS,x)
+        l = diffs.shape[0]
+        norm = np.zeros(l)
+        
+        for i in range(l):
+            norm[i] = np.linalg.norm(diffs[i,:])
+        print(norm)
+        if(np.amin(norm) <= 10000):
+            return False
+        else:
+            SLIDEENDS.append(y)
+            return True
+    print(RUN)
+    RUN += 1
+
+        
+
 def main():
     ### video capture to get fps, # of frames, and first frame ###
-    vidcap = cv2.VideoCapture('Lec_examp.mp4')
+    vidcap = cv2.VideoCapture('zoom_0.mp4')
     success = True
 
     # interval = 5610 # just to take screenshot at 6:14 mark of video
@@ -15,33 +44,36 @@ def main():
 
     numsec = frames//fps  # duration of video (sec)
 
-
     rval, frame = vidcap.read()  # gets first frame
     first_frame = frame
     vidcap.release()
 
-
     manual = 12*60 + 29
     print(numsec, 'seconds, ', manual,'seconds, ',frames, 'frames, ', fps, 'fps')
 
-    plt.imshow(first_frame)
+    #plt.imshow(first_frame)
 
-    ### converting image into grayscale image (np array) ###
+    ## // New worse converting image into grayscale image (np array) ###
+    # gray_ff = cv2.cvtColor(first_frame,cv2.COLOR_RGB2GRAY)
+    # gff = np.array(gray_ff)
+    
+
+    ### OLD better converting image into grayscale image (np array) ###
     ff = np.array(first_frame)
     d = np.array([0.2989, 0.5870, 0.1140])
-
     gff = np.dot(ff, d) #grayscale version of first frame
 
-    ### plotting in jupyter notebook ###
+    ##// plotting in jupyter notebook ###
     # fig, (ax1, ax2) = plt.subplots(2, 1)
     # ax1.imshow(ff)
     # ax2.imshow(gff,cmap = plt.get_cmap("gray"))
 
+
     scr_shot = gff
-    interval = 15                              # take screenshot every second
+    interval = fps                             # take screenshot every second
 
     ### video capture to go through whole video
-    vidcap = cv2.VideoCapture('Lec_examp.mp4')
+    vidcap = cv2.VideoCapture('zoom_0.mp4')
     success = True 
     # start = 0   # variable to check if this is the first frame
     timestamps = [] # list to add time stamps and norm values to
@@ -52,19 +84,28 @@ def main():
     while success:
         success, image = vidcap.read()
         if (f%interval == 0):
+            ## // New Worse convert to gray ###
+            # gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+            # gnew = np.array(gray) # grayscale numpy version of image
+            
+
+            ### old better convert to gray ###
             new = np.array(image)
             gnew = np.dot(new, d) # grayscale numpy version of image 
+
             diff = np.subtract(scr_shot, gnew) # difference between two images
             norm = np.linalg.norm(diff) # norm of difference
             timestamps.append(((prev//fps, f//fps), norm))
 
             if (norm >= 10000):
-                start_time_min = int((prev//fps)//60)
-                start_time_sec = int((prev//fps)-(60*start_time_min))
-                end_time_min = int((f//fps)//60)
-                end_time_sec = int((f//fps)-(60*end_time_min))
-                mmss = f"{start_time_min}:{start_time_sec} - {end_time_min}:{end_time_sec}" 
-                slides.append(mmss)
+                check = (new_checks(gnew,scr_shot))
+                if(check == True):
+                    start_time_min = int((prev//fps)//60)
+                    start_time_sec = int((prev//fps)-(60*start_time_min))
+                    end_time_min = int((f//fps)//60)
+                    end_time_sec = int((f//fps)-(60*end_time_min))
+                    mmss = f"{start_time_min}:{start_time_sec} - {end_time_min}:{end_time_sec}" 
+                    slides.append(mmss)
 
             prev = f
             
@@ -73,7 +114,7 @@ def main():
         
         
     ### create txt file of frames and norms ###    
-    with open("norms_1.3.txt", "w") as output:
+    with open("norms_2.0.txt", "w") as output:
         for listitem in timestamps:
             output.write('%s\n' % str(listitem))
 
