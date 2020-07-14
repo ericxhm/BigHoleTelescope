2b57af653ad7f3ac8a190066725fef36d6fda480
Eric Ma <exm4@cornell.edu>
2020-07-14 16:11:05 -0400

    10 second divisions

--- transcript.py
+++ transcript.py
@@ -7,7 +7,8 @@ from pydub import AudioSegment
 fileName = sys.argv[1]
 fullAudio = "fullAudio.wav"
 tempAudio = "tempAudio.wav"
-div = 60000 #clip division in milliseconds
+div = 10000 #clip division in milliseconds
+ 
 
 if fileName[len(fileName)-4] == ".":
     txtfile = fileName[:-4] + ".txt"
@@ -26,17 +27,22 @@ text = open(txtfile,"w+")
 
 
 r = sr.Recognizer()
-
+print("Number of clips: " + str(clips))
 for x in range(clips+1):
+    begValue = x*div
     if x == clips:
-        clipAudio = audio[(x*div):]
+        clipAudio = audio[(begValue):]
     else:
-        clipAudio = audio[(x*div):(x+1)*div-1]
+        clipAudio = audio[(begValue):(x+1)*div-1]
     clipAudio.export(tempAudio,format="wav")
+
+    minute = begValue//60000
+    second = (begValue%60000)//1000
 
     with sr.AudioFile(tempAudio) as source: #ISSUE HERE, find way to not have to write a file every time
         audioOut = r.record(source)
-    text.write(r.recognize_google(audioOut))
+    text.write(str(minute) + ":" + f"{second:02d}"+ "    " + r.recognize_google(audioOut) + "\n\n")
+    print("Current clip: " + str(x),end="\r")
 
 text.close
 
